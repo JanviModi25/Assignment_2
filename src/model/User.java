36eed0f963e3e46bef220e09cd074854e5da4c36
Janvi Daxesh Modi <modi.ja@northeastern.edu>
2022-10-06 11:03:11 +0530

    Created ui and model package

---
+++ src/model/User.java
@@ -0,0 +1,121 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class User {
+    private String Name;
+    private String EmpID;
+    private String Age;
+    private String Gender;
+    private String SrtDt;
+    private String Level;
+    private String TeamInfo;
+    private String Postitle;
+    private String ContInfo;
+    private String CellPhnNo;
+    private String emailadd;
+    private String Photo;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getEmpID() {
+        return EmpID;
+    }
+
+    public void setEmpID(String EmpID) {
+        this.EmpID = EmpID;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getSrtDt() {
+        return SrtDt;
+    }
+
+    public void setSrtDt(String SrtDt) {
+        this.SrtDt = SrtDt;
+    }
+
+    public String getLevel() {
+        return Level;
+    }
+
+    public void setLevel(String Level) {
+        this.Level = Level;
+    }
+
+    public String getTeamInfo() {
+        return TeamInfo;
+    }
+
+    public void setTeamInfo(String TeamInfo) {
+        this.TeamInfo = TeamInfo;
+    }
+
+    public String getPostitle() {
+        return Postitle;
+    }
+
+    public void setPostitle(String Postitle) {
+        this.Postitle = Postitle;
+    }
+
+    public String getContInfo() {
+        return ContInfo;
+    }
+
+    public void setContInfo(String ContInfo) {
+        this.ContInfo = ContInfo;
+    }
+
+    public String getCellPhnNo() {
+        return CellPhnNo;
+    }
+
+    public void setCellPhnNo(String CellPhnNo) {
+        this.CellPhnNo = CellPhnNo;
+    }
+
+    public String getEmailadd() {
+        return emailadd;
+    }
+
+    public void setEmailadd(String emailadd) {
+        this.emailadd = emailadd;
+    }
+
+    public String getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(String Photo) {
+        this.Photo = Photo;
+    }
+    
+}
